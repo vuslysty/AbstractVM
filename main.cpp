675c5bd67a4bf4cad1de133f81641f95ddd445b9
Vlad <vladuvi23@gmail.com>
2019-07-19 22:19:12 +0300

    regexp which prepare file to validation and split to vector of instructions

--- main.cpp
+++ main.cpp
@@ -7,23 +7,39 @@
 #include <regex>
 #include <iterator>
 
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> split(const std::string &s, char delim) {
+	std::stringstream			ss(s);
+	std::string					item;
+	std::vector<std::string>	elems;
+
+	while (std::getline(ss, item, delim))
+		elems.push_back(item);
+	return elems;
+}
+
 int main ()
 {
 	std::string s (
 			"  		\n\n\n\npush 	int8     (23      )         	 \n"
    			"add	; sadfjsaldfj\n  \n  		 	 "
    			"pop;sdafasdf asdf asd fasdf\n"
-   			"print 	 \n    \n    \n\n\n	 	; asdfasdf	 "
+   			"print 	 \n    \n    \n\n\n	 	; asdfasdf	\n\n\n\n\n "
 	);
 
+	// delete comments
 	s = std::regex_replace (s, std::regex(";[^\n]*") ,"");
 
+	// delete white spaces befor and after OPEN_SCOPE | CLOSE_SCOPE
 	s = std::regex_replace (s ,std::regex("[\t ]*\\([\t ]*") ,"(");
-
 	s = std::regex_replace (s, std::regex("[\t ]*\\)[\t ]*"), ")");
 
+	// delete white spaces befor ENDLINE
 	s = std::regex_replace (s, std::regex("(\n[\t ]*)"), "\n");
-
+	// delete white spaces after ENDLINE
 	s = std::regex_replace (s, std::regex("([\t ]*\n)"), "\n");
 
 	// delete empty rows
@@ -38,8 +54,10 @@ int main ()
 	// delete multiple white spaces
 	s = std::regex_replace (s, std::regex("[\t ]{2,}"), " ");
 
-	std::cout << s;
+	std::vector<std::string> lines = split(s, '\n');
 
+	for(unsigned int i = 0; i < lines.size(); i++)
+		std::cout << lines[i] << '\n';
 
 	return 0;
 }
